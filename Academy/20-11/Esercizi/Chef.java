8c046572b05ab638c15f71fa11a7233744e5b127
giovannifelle <giovannifelle2@gmail.com>
2024-11-20 11:50:37 +0100

    11:50 20-11

---
+++ Academy/20-11/Esercizi/Chef.java
@@ -0,0 +1,34 @@
+package Esercizi;
+
+import java.util.ArrayList;
+
+public class Chef {
+
+    public String nome;
+    public String tipoChef;
+    public ArrayList<String> ingredienti;
+
+    public Chef(String nome, String tipoChef, ArrayList<String> ingredienti) {
+        this.nome = nome;
+        this.tipoChef = tipoChef;
+        this.ingredienti = ingredienti;
+    }
+
+    public void creaPiatto(){
+        for(String ingrediente: ingredienti){
+            ingrediente=ingrediente.toLowerCase();
+            switch (ingrediente){
+                case "patate":
+                    System.out.println("Crea patate al forno");
+                    break;
+                case "carne":
+                    System.out.println("Crea carne alla griglia");
+                    break;
+                case "pomodiri":
+                    System.out.println("Crea insalata");
+                    break;
+            }
+        }
+    }
+}
+
--- Academy/20-11/Main.java
+++ Academy/20-11/Main.java
@@ -1,5 +1,6 @@
+import Esercizi.Libro;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("CIao");
     }
 }
