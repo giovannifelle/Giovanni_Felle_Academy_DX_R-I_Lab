cd3382fc1643a152773e196ee1fb36397e10b221
giovannifelle <giovannifelle2@gmail.com>
2024-11-25 16:12:21 +0100

    Start book-service

---
+++ Academy/25-11/book-service/src/main/java/it/eng/corso/bookservice/model/Book.java
@@ -0,0 +1,23 @@
+package it.eng.corso.bookservice.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public String title;
+    public String author;
+    public Double price;
+}
