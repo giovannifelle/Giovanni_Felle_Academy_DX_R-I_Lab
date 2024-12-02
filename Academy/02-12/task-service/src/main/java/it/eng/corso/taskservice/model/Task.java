83a8bb28f8e4a418cc389bed426abab14271167a
giovannifelle <giovannifelle2@gmail.com>
2024-12-02 14:45:54 +0100

    Fine Task-service

--- Academy/02-12/task-service/src/main/java/it/eng/corso/taskservice/model/Task.java
+++ Academy/02-12/task-service/src/main/java/it/eng/corso/taskservice/model/Task.java
@@ -15,7 +15,7 @@ public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+//Commento
     private String title;
     private String description;
     private Boolean completed;
